5b703f0391f2297950067cbb349c576865d83b38
cogsincogs <jamiewthomas13@gmail.com>
2023-01-19 15:56:40 +0000

    Create genres page with twig

--- classes/albumController.class.php
+++ classes/albumController.class.php
@@ -10,6 +10,22 @@ class AlbumController extends Dbh {
         return $stmt->fetchAll();
     }
 
+    public function getProperty($id, $property) {
+        $id = htmlspecialchars($id);
+        $property = htmlspecialchars($property);
+        $sql = "SELECT $property FROM albums WHERE ID = $id";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    public function getCount() {
+        $sql = "SELECT COUNT(id) FROM albums";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
     public function setAlbum($title, $artist, $year, $genre, $tracks, $imageURL, $rating) {
         $sql = "INSERT INTO albums (Title, Artist, Year, Genre, Tracks, AlbumArtURL, Rating) VALUES (:title, :artist, :year, :genre, :tracks, :imageURL, :rating);";
         $stmt = $this->connect()->prepare($sql);
